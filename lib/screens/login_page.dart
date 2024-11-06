f184a2828f3110d4dff0b4748c26531ed0e1139d
lakshay singhal <91775547+plakshay@users.noreply.github.com>
2024-11-07 03:47:12 +0530

    day 6 done

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:target10days/utils/routes.dart';
-class LoginPage extends StatelessWidget {
+ class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name="";
+  bool changeButton = false; // this is very important, false by default, we will change its state to true and apply conditional statement in every button operation
+  @override
+  
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child : SingleChildScrollView(
+      child : SingleChildScrollView( // single child scroll view is used in order to scroll thorugh the pages and avoid the overflow or distortion of UI in smaller screens
         child: Column(
           children: [
             Image.asset("assets/images/login.png"),
             Text(
-              "Welcome",
+              "Welcome $name",
+              // $ name is used in order to display the name dynamically after welcome
               style: TextStyle(
                 fontSize: 40,
                 fontWeight: FontWeight.bold,
@@ -30,6 +39,14 @@ class LoginPage extends StatelessWidget {
                       hintText: "Enter Username",
                       labelText: "Username",
                     ),
+                    onChanged: (value){
+                      // this is used to apply a changed value of name, value is taken as an input argument in the onchanged function which is passed to name
+                      name=value; 
+                      setState(() {
+                        // setstate is used to call the build function again and helps to change the state of a specific thing
+                      });
+                    },
+                     
                   ),
                   TextFormField(
                     obscureText: true,
@@ -41,14 +58,46 @@ class LoginPage extends StatelessWidget {
                   SizedBox(
                     height: 50,
                   ),
-                  ElevatedButton(
-                    child: Text("Login"),
-                    style: TextButton.styleFrom(minimumSize: Size(100, 50)),
-                    onPressed: () {
+
+                    // login button
+                  InkWell( //to make it clickable : two options- 1. inkwell  2. gesturedetector - gives no response/ no ripple effect in the UI
+                    onTap: () async {
+                      setState(() {
+                        changeButton=true;
+                      });
+                      await Future.delayed(Duration(seconds: 1));// await is used to delay the navigator operation by 1 second using asynchronus function
                       Navigator.pushNamed(context, MyRoutes.homeroute);
-                    }, 
-                  
+                    },
+                    
+                    child: AnimatedContainer(
+                      // to make the button animated we used AnimateContainer 
+                      duration: Duration(seconds: 1 ),// duration for animation/ after how many seconds will the animation happen
+                      width: changeButton? 50 : 150,
+                      height:   50,
+                      alignment: Alignment.center,
+                      child: changeButton? Icon(Icons.done,color: Colors.white,): Text(
+                        "Login",
+                        style: TextStyle(
+                          color:Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius: BorderRadius.circular(changeButton? 50:8),
+                        ),
+                    ),
                   )
+                  // ElevatedButton(
+                  //   child: Text("Login"),
+                  //   style: TextButton.styleFrom(minimumSize: Size(100, 50)),
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context, MyRoutes.homeroute);
+                  //     // navigator is used to navigate to different pages that are routed
+                  //   }, 
+                  
+                  // ) commented in order to make an animation for button 
                 ],
               ),
             ),
